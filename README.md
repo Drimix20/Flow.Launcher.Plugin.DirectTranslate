dc0277345dd64e2dfddad42ec85cf5aea40f5280
Martin Drimal <martin.drimal@mavenir.com>
2021-03-26 06:48:09 +0100

    Migrated DirectTranslate to Flow Launcher

---
+++ README.md
@@ -0,0 +1,17 @@
+# Translate
+# Plugin for [Flow Launcher](https://flow-launcher.github.io/docs/#/) that translates between any languages supported by python textblob library.
+
+![Translate](https://i.imgur.com/mVxnBk1.gif)
+
+Pressing ```Enter``` will redirect to a Google Translate page with the query.
+List of supported languages can be found [here](https://cloud.google.com/translate/docs/languages)
+
+
+## Installation
+[Get FLow Launcher](https://github.com/Flow-Launcher/Flow.Launcher)
+
+Install [TextBlob](https://textblob.readthedocs.io/en/dev/contributing.html):
+```
+pip install textblob
+```
+To install the plugin, download plugin dir and place into Flow Launcher's plugin directory. Restart Flow Launcher.
---
+++ main.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from textblob import TextBlob, exceptions
+from flowlauncher import FlowLauncher
+
+AUTOMATIC_LANGUAGES = 'cs'
+
+def translate(query):
+    query_modified = query.strip().lower()
+    splitted = query_modified.split(' ')
+    from_lang = splitted[0]
+    to_lang = splitted[1]
+    
+    results = []
+
+    en = set(chr(i) for i in range(ord('a'), ord('z') + 1))
+    
+    if query_modified:
+        try:
+            blob = TextBlob(' '.join(splitted[2:]))
+            translation = blob.translate(from_lang, to_lang)
+            results.append({
+                "Title": str(translation),
+                "SubTitle": query,
+                "IcoPath":"Images/app.png"
+            })
+        except exceptions.NotTranslated:
+            results.append({
+                "Title": 'Query not changed',
+                "SubTitle": query,
+                "IcoPath":"Images/app.png"
+            })
+    if not results:
+        results.append({
+                "Title": 'Not found',
+                "SubTitle": query,
+                "IcoPath":"Images/app.png"
+            })
+    return results
+
+class Translate(FlowLauncher):
+    
+    def query(self, query):
+        return translate(query)
+        
+    def context_menu(self, data):
+        return []
+
+
+if __name__ == "__main__":
+    Translate()
