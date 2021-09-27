54432f1378cce4d8ad029f6ba0a0df44eeea7410
Drimix20 <drimalmartin@seznam.cz>
2021-09-27 19:51:11 +0200

    Fix list index out of range

--- main.py
+++ main.py
@@ -7,28 +7,29 @@ AUTOMATIC_LANGUAGES = 'cs'
 def translate(query):
     query_modified = query.strip().lower()
     splitted = query_modified.split(' ')
-    from_lang = splitted[0]
-    to_lang = splitted[1]
     
     results = []
+    if len(splitted) > 3:
+        from_lang = splitted[0]
+        to_lang = splitted[1]
 
-    en = set(chr(i) for i in range(ord('a'), ord('z') + 1))
-    
-    if query_modified:
-        try:
-            blob = TextBlob(' '.join(splitted[2:]))
-            translation = blob.translate(from_lang, to_lang)
-            results.append({
-                "Title": str(translation),
-                "SubTitle": query,
-                "IcoPath":"Images/app.png"
-            })
-        except exceptions.NotTranslated:
-            results.append({
-                "Title": 'Query not changed',
-                "SubTitle": query,
-                "IcoPath":"Images/app.png"
-            })
+        en = set(chr(i) for i in range(ord('a'), ord('z') + 1))
+        
+        if query_modified:
+            try:
+                blob = TextBlob(' '.join(splitted[2:]))
+                translation = blob.translate(from_lang, to_lang)
+                results.append({
+                    "Title": str(translation),
+                    "SubTitle": query,
+                    "IcoPath":"Images/app.png"
+                })
+            except exceptions.NotTranslated:
+                results.append({
+                    "Title": 'Query not changed',
+                    "SubTitle": query,
+                    "IcoPath":"Images/app.png"
+                })
     if not results:
         results.append({
                 "Title": 'Not found',
