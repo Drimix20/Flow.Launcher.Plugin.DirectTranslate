baf87c0562b6d45bfc7a8b3316ad24a8c8e0cd05
Damien <damien@frawley.id.au>
2021-12-09 22:31:06 +1100

    add code to point python to correct dir

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-basedirectory = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(basedirectory)
+import sys, os
+
+parent_folder_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(parent_folder_path)
+sys.path.append(os.path.join(parent_folder_path, "lib"))
+sys.path.append(os.path.join(parent_folder_path, "plugin"))
 
 # -*- coding: utf-8 -*-
 
