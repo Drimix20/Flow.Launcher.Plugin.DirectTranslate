017b7e01cda08abcfda0dc6ff0f710532a06819f
fabio <fabio@chelly.net>
2022-04-08 11:38:10 +0200

    Translating from <auto> to <system language> if no lang has been provided.
    Using google table of languages to check if parameter is a valid language.
    
    Detecting language before translation to be able to display the right language if "auto" has been used.

--- plugin/ui.py
+++ plugin/ui.py
@@ -6,12 +6,18 @@ from typing import List
 from flowlauncher import FlowLauncher
 
 from googletrans import Translator
+from googletrans.constants import LANGUAGES, SPECIAL_CASES
 from plugin.templates import *
 from plugin.extensions import _
+import locale
 
 
 class Main(FlowLauncher):
     messages_queue = []
+
+    def getSystemLanguage(self):
+        lang = locale.getdefaultlocale()
+        return lang[0][:2] if lang else "en"
 
     def sendNormalMess(self, title: str, subtitle: str):
         message = copy.deepcopy(RESULT_TEMPLATE)
@@ -34,38 +40,39 @@ class Main(FlowLauncher):
 
         self.messages_queue.append(message)
 
+    @staticmethod
+    def valid_lang(lang: str) -> bool:
+        return lang in LANGUAGES or lang in SPECIAL_CASES
+
     def query(self, param: str) -> List[dict]:
-        query = param.strip()
+        query = param.strip().lower()
+        params = query.split(" ")
+        cnt_words = len(params)
 
-        query_modified = query.strip().lower()
-        splitted_params = query_modified.split(' ')
-
-        if len(splitted_params) > 1 and len(splitted_params[0]) == 2 and len(splitted_params[1]) != 2:
-            # There is only the destination lang: we can assume that the from lang is "auto"
-            query_modified = f"auto {query_modified}"
-            splitted_params = query_modified.split(' ')
-
-        if len(splitted_params) < 3:
-            self.sendNormalMess(
-                "Direct Translate",
-                _("<Hotkey> <From Language> <To Language> <Text>")
-            )
+        if cnt_words <= 1:
+            self.sendNormalMess("Direct Translate", _("<Hotkey> <From Language> <To Language> <Text>"))
         else:
-            from_lang = splitted_params[0]
-            to_lang = splitted_params[1]
-            
+            if not self.valid_lang(params[0]):
+                # no lang_code provided: <auto> -> <system default language>
+                lang1, lang2 = "auto", self.getSystemLanguage()
+            else:
+                if not self.valid_lang(params[1]):
+                    # one lang_code provided: <auto> -> lang_code
+                    lang1, lang2 = "auto", params[0]
+                    query = " ".join(params[1:])
+                else:
+                    # 2 lang_codes provided: lang_code -> lang_code
+                    lang1, lang2 = params[:2]
+                    query = " ".join(params[2:])
+
             try:
                 translator = Translator()
-                translation = translator.translate(' '.join(splitted_params[2:]), src=from_lang, dest=to_lang)
+                if lang1 == "auto":
+                    lang1 = translator.detect(query).lang
 
-                self.sendNormalMess(
-                    _(str(translation.text)),
-                    query
-                )
+                translation = translator.translate(query, src=lang1, dest=lang2)
+                self.sendNormalMess(_(str(translation.text)), f"{query}   [{lang1} → {lang2}]")
             except ValueError as error:
-                self.sendNormalMess(
-                    _(str(error)),
-                    query
-                ) 
+                self.sendNormalMess(_(str(error)), query)
 
         return self.messages_queue
